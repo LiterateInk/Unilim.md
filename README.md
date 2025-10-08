526e867fb1a0a77176529739360ac9769d33d979
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-10-09 00:54:17 +0200

    feat: add starlight documentation

--- README.md
+++
@@ -1,5 +0,0 @@
-# Unilim's Protocol for APIs
-
-## CAS (`cas.unilim.fr`)
-
-See [CAS.md](CAS.md) for more information.
---
+++ astro.config.mjs
@@ -0,0 +1,65 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
+
+import starlight from "@astrojs/starlight";
+import starlightSidebarTopics from "starlight-sidebar-topics";
+
+export default defineConfig({
+  adapter: netlify(),
+  integrations: [
+    starlight({
+      title: "Unilim",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/LiterateInk?q=Unilim",
+        },
+        {
+          icon: "discord",
+          label: "Discord",
+          href: "https://literate.ink/discord",
+        },
+      ],
+      plugins: [
+        starlightSidebarTopics([
+          {
+            label: "Guides",
+            link: "/guides/getting-started/installation",
+            icon: "open-book",
+            items: [
+              {
+                label: "Getting Started",
+                autogenerate: { directory: "guides/getting-started" },
+              },
+              {
+                label: "Examples",
+                autogenerate: { directory: "guides/examples" },
+              },
+            ],
+          },
+          {
+            label: "References",
+            link: "/references",
+            icon: "document",
+            items: [
+              {
+                label: "Overview",
+                link: "/references",
+              },
+              {
+                label: "JS/TS",
+                link: "https://js.unilim.docs.literate.ink/",
+              },
+              {
+                label: "Rust",
+                link: "https://docs.rs/unilim/",
+              },
+            ],
+          },
+        ]),
+      ],
+    }),
+  ],
+});
